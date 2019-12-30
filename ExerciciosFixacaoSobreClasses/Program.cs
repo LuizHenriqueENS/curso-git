3b1769c700874a0bfd1247a5cd39b1f73422a6a1
Luiz Henrique <luizhenrique.egydio@gmail.com>
2019-12-30 16:15:59 -0300

    Projeto inciado, mostrando exercicios realizados

---
+++ ExerciciosFixacaoSobreClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosFixacaoSobreClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+           
+            Console.Write("ENTRE COM A LARGURA E A ALTURA DE UM RETANGULO \nLARGURA:");
+            ret.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("ALTURA: ");
+            ret.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            double area = ret.Area();
+            double perimetro = ret.Perimetro();
+            double diagonal = ret.Diagonal();
+
+
+            Console.WriteLine(ret);
+
+        }
+    }
+}
---
+++ ExerciciosFixacaoSobreClasses/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosFixacaoSobreClasses
+{
+    class Retangulo
+    {
+        public double A;
+        public double B;
+
+        public double Area()
+        {
+            return A * B;
+        }
+
+        public double Perimetro()
+        {
+            return 2*(A + B);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(A, 2) + Math.Pow(B, 2));
+        }
+
+        public override string ToString()
+        {
+            return "AREA: "
+                + Area().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nPERÍMETRO: "
+                + Perimetro().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nDiagonal: "
+                + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
